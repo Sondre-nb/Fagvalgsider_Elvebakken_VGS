5ee529fdc88cf17a26ea2a8ba8abd647a7217451
Sondre <sondre.njastad.bjorn@gmail.com>
2024-10-31 09:20:10 +0100

    Fungerende scroll med knapper og manuell scrolling
    Knappene forsvinner ikke når man ikke kan bla langer til den siden de peker, og om man scroller manuelt slik at man ser halvparten av et fag, vil pilene gjøre at man ser den andre halvparten pluss litt, men ikke hele faget. Akkurat dette er mulig å beholde, siden det stort sett er oversiktlig uansett.

--- script/global.js
+++ script/global.js
@@ -1,36 +1,15 @@
 // blar i menyen over fagvalgene
 function flyttMeny(retning){
     skjermbredde = document.documentElement.clientWidth;
-    let fag_meny = document.getElementById("fag_meny");
-    let fag_meny_bredde = 2*15 + 16*200 // 2 ganger padding (15) + antall lenker(16) ganger plassen hver lenke tar (200)
-
-    //henter variabel
-    let r = document.querySelector(':root');
-    let rs = getComputedStyle(r);
-    let marginVenstre = rs.getPropertyValue('--margin_venstre_tall');
-
+    const fag_meny = document.getElementById("fag_meny");
     // regner ut hvor langt menyen skal bla om man trykker på knappene
-    let blaLengde = skjermbredde - (skjermbredde % 200); // 200px er hvor mye plass hver lenke tar
+    const blaLengde = skjermbredde - (skjermbredde % 200); // 200px er hvor mye plass hver lenke tar
     
     if (retning == 'høyre'){
-        //Endrer margin-left og blar til høyre
-        marginVenstre -= blaLengde
-
-        //Hindrer menyen i å bla for langt til høyre
-        if (marginVenstre < -(fag_meny_bredde - skjermbredde)){
-            marginVenstre = -(fag_meny_bredde - skjermbredde);
-        }
+        // blar til høyre
+        fag_meny.scrollBy({ left: blaLengde, behavior: 'smooth' });
     } else{
-        //Endrer margin-left og blar til venstre
-       marginVenstre -= -blaLengde;
-
-        // hindrer menyen i å bla for langt til venstre 
-        if (marginVenstre > 0){
-            marginVenstre = 0;
-        }
+        // blar til venstre
+        fag_meny.scrollBy({ left: -blaLengde, behavior: 'smooth' });
     }
-    // Oppdaterer margin
-    r.style.setProperty('--margin_venstre_tall', marginVenstre); 
-    // overgangen tar 0,8 sek 
-    fag_meny.style.transitionDuration = "0.8s";
 }
