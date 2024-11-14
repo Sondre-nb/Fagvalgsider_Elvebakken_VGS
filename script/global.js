37f97decabc375b8894af6edc9b55a0eb1ac6c5b
JulieDenKulie <julie-stang@outlook.com>
2024-11-14 08:29:37 +0100

    Kommenterte og fikset kjemi-side
    la til flere bokser og sånt

--- script/global.js
+++ script/global.js
@@ -71,10 +71,9 @@ function toggleMeny(){
     }
 }
 
-
 // Gradvis skjuler knappen ned fra statskjermen
 window.addEventListener("scroll", function () {
-    let header = document.getElementById("knapp");
+    let header = document.getElementById("ned-knapp");
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     let scrollBunn = window.innerHeight / 2
     header.style.opacity = 1 - scrollTop / scrollBunn;
