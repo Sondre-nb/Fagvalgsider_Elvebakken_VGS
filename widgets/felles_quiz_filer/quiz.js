0dd0e318165ff994a974c94dec5106a267da6188
Sondre <sondre.njastad.bjorn@gmail.com>
2024-11-24 14:42:00 +0100

    Sortering av kode

--- widgets/felles_quiz_filer/quiz.js
+++ widgets/felles_quiz_filer/quiz.js
@@ -1,10 +1,3 @@
-let spørsmålListe = [
-                    {"spørsmål":"Hva er 1+1?", "alternativer":[1, 2, 3, 4], "svar":2},
-                    {"spørsmål":"Hva er 1+2?", "alternativer":["Weimarrepublikken sitt mareritt", 3, 2, 100], "svar":3},
-                    {"spørsmål":"Hva er hovedstaden i Oslo?", "alternativer":["Weimarrepublikken", "Røa", "Hovseter", "Sarabråtveien 11B :("], "svar":"Hovseter"},
-                    {"spørsmål":"Hva er hovedstaden i Bergen?", "alternativer":["Weimarrepublikken", "Røa", "Bergen", "Njåstadveien"], "svar":"Weimarrepublikken"}
-                    ];
-
 let spørsmålNr = 0;
 let poeng = 0;
 let valgt_alernativ = "ingen";
