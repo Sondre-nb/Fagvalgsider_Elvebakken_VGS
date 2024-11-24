416dc1fd37ba27ec9a898820056937b8c8c2c6ed
JulieDenKulie <julie-stang@outlook.com>
2024-11-24 22:40:50 +0100

    Kjemi-spill er kommentert
    Hvem skulle trodd!? (Ikke jeg :))

--- widgets/drag_and_drop_kjemi_spill/drag_and_drop_kjemi_spill.js
+++ widgets/drag_and_drop_kjemi_spill/drag_and_drop_kjemi_spill.js
@@ -4,27 +4,30 @@ let svarbokser = document.getElementsByClassName("svar-boks");
 let sjekk_svar = document.getElementById("sjekk-svar");
 let nullstill = document.getElementById("nullstill");
 
+// Lar deg dra påstander inn og ut av svarboksene 
+// Sjekker om hver av påstandene blir dratt
 for (påstand of påstander) {
     påstand.addEventListener("dragstart", function(e){
         for (let i = 0; i < svarbokser.length; i++) {
-            svarbokser[i].style.backgroundColor = "white"; // nullstiller farger om man har sjekket svar
+            svarbokser[i].style.backgroundColor = "white"; // Nullstiller farger om man har sjekket svar
         }
-        let selected = e.target;
-        selected.style.filter = "brightness(80%)"; // gjør at drag-element ikke arver bakgrunnsfarge fra div
-        selected.style.opacity = 100;
+        let selected = e.target; // diven med teksten man drar på
+        selected.style.filter = "brightness(80%)"; // Gjør at drag-element ikke arver bakgrunnsfarge fra div
         for (svarboks of svarbokser) {
             svarboks.addEventListener("dragover", function(e){
                 e.preventDefault();
             });
-            svarboks.addEventListener('dragenter', function(e) {
+            // Legger til hover-effekt på den boksen man drar tekst over
+            svarboks.addEventListener('dragenter', function() {
                 if (this.innerHTML == "") {
                     this.style.backgroundColor = "#dddddd";
                 }
             })
-            svarboks.addEventListener('dragleave', function(e) {
+            svarboks.addEventListener('dragleave', function() {
                 this.style.backgroundColor = "white";
             })
-            svarboks.addEventListener('drop', function(e){
+            // Plasserer div-en inni svar-boks om den er tom
+            svarboks.addEventListener('drop', function(){
                 if (this.innerHTML == "") {
                     this.appendChild(selected)
                 }
@@ -32,27 +35,31 @@ for (påstand of påstander) {
                 this.style.backgroundColor = "white";
             });
         }
+        // Lar deg dra tekst ut av svar-boksene igjen
         påstand_boks.addEventListener("dragover", function(e){
             e.preventDefault();
         });
-        påstand_boks.addEventListener('drop', function(e){
+        påstand_boks.addEventListener('drop', function(){
             this.appendChild(selected);
             selected = null;
         });
     })
 }
 
+// Sjekker om 'sjekk svar!'-knappen er trykket
 sjekk_svar.addEventListener("click", function () {
+    // Svarene er i samme rekkefølge som kategoriene
     let svar = [
         '"Hvis jeg blander to mol hydrogengass med ett mot oksygengass, får jeg dannet ett mol vann"',
         '"Når vann fryser til is, gir det fra seg varme"',
         '"Hvis jeg blander eddik og natron får jeg dannet vann og karbondioksid"',
         '"Etanol har en OH-gruppe og er derfor blandbar med vann"'
     ];
+    // Endrer bakgrunnsfarge på svar-bokser basert på om svar er riktig eller ikke
     for (let i = 0; i < påstander.length; i++) {
         if (svarbokser[i].innerHTML != "") {
             if (svar[i] == svarbokser[i].querySelector('.påstand').innerHTML) {
-                svarbokser[i].style.backgroundColor = "#ccffc5";
+                svarbokser[i].style.backgroundColor = "#CCFFC5";
             } else {
                 svarbokser[i].style.backgroundColor = "#FFD9C5";
             }
@@ -62,6 +69,9 @@ sjekk_svar.addEventListener("click", function () {
     }
 });
 
+// Sjekker om 'nullstill'-knappen er trykket
+// Fjerner farge fra svar-bokser
+// Putter all tekst tilbake i påstand-boks
 nullstill.addEventListener("click", function () {
     for (let i = 0; i < svarbokser.length; i++) {
         svarbokser[i].style.backgroundColor = "white";
