bb213edd95b7351fab3ae053db863935b153c6c2
Sondre <sondre.njastad.bjorn@gmail.com>
2024-12-10 20:10:28 +0100

    Fikset footer og håper jeg ikke trenger å være trist lenger

--- script/footer.js
+++ script/footer.js
@@ -1,4 +1,4 @@
-fetch('../widgets/footer.html')
+fetch('/Fagvalgsider_Elvebakken_VGS/widgets/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-placeholder').innerHTML = data;
