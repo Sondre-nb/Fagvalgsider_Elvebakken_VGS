4a0d54d506e3a63e20edbac3872325244c6a8721
Trym Bringsrud Fekjær <trym.mcduck@gmail.com>
2024-12-10 15:32:03 +0100

    hm

--- script/footer.js
+++ script/footer.js
@@ -1,4 +1,4 @@
-fetch('/widgets/footer.html')
+fetch('../widgets/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-placeholder').innerHTML = data;
