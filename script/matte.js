1109bd3d3542e2767350ebc77076f437b77a9f36
Sondre <sondre.njastad.bjorn@gmail.com>
2024-12-14 21:55:59 +0100

    Simpel start på idrettsfag og kanskje fikset matte-lenke tilbake til ssø

--- script/matte.js
+++ script/matte.js
@@ -3,5 +3,5 @@ window.addEventListener("DOMContentLoaded", function(){
     let side_man_kommer_ifra = String(localStorage.getItem("sideManKommerIfraLagring")).replace(/['"]+/g, ''); //.replace fjerner hermetegn
     tilbakeKnappEl = document.querySelector("#tilbake-knapp");
     tilbakeKnappEl.innerHTML = "&lt; Tilbake til " + side_man_kommer_ifra;
-    tilbakeKnappEl.href = "../../" + side_man_kommer_ifra + "/";
+    tilbakeKnappEl.href = "../../" + side_man_kommer_ifra.toLowerCase() + "/";
 });
