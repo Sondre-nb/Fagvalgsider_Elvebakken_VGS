f2c2ee9472cc3e582ba8cdd744bd013f374bb0cd
Sondre <sondre.njastad.bjorn@gmail.com>
2024-12-15 20:08:37 +0100

    Fjernet falske sitater og la til alt-tekst på alle programfagene frem til rettslære(alfabetisk)

--- script/footer.js
+++ script/footer.js
@@ -1,4 +1,4 @@
-fetch('/Fagvalgsider_Elvebakken_VGS/widgets/footer.html')
+fetch('../../widgets/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-placeholder').innerHTML = data;
