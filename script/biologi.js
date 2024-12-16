193ce25ee5dba1d04d7513e1f59b59579a8c05d9
JulieDenKulie <julie-stang@outlook.com>
2024-12-16 18:16:47 +0100

    Endret til på css-filene dine, Trym
    Bare sånn at ting ser litt bedre ut :)

--- script/biologi.js
+++ script/biologi.js
@@ -1,4 +1,4 @@
-window.onresize = function() {
+function skalerIframe() {
     let iframe = document.getElementById("memory-match-spill");
     let bunn = iframe.contentWindow.document.getElementById("nullstill-knapp").getBoundingClientRect().top;
     iframe.style.height = bunn + 70 + "px";
@@ -7,3 +7,6 @@ window.onresize = function() {
 let iframe = document.getElementById("memory-match-spill");
 let bunn = iframe.contentWindow.document.getElementById("nullstill-knapp").getBoundingClientRect().top;
 iframe.style.height = bunn + 70 + "px";
+
+document.addEventListener("DOMContentLoaded", skalerIframe);
+window.addEventListener("resize", skalerIframe);
