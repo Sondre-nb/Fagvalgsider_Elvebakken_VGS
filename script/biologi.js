64cc9d8235e6d83448960d8c7d3fbe7ed096f1c4
Sondre <sondre.njastad.bjorn@gmail.com>
2024-12-16 21:33:07 +0100

    Fjernet unødvendig kode som kanskje fikset bug biologi iframe

--- script/biologi.js
+++ script/biologi.js
@@ -4,9 +4,5 @@ function skalerIframe() {
     iframe.style.height = bunn + 70 + "px";
 }
 
-let iframe = document.getElementById("memory-match-spill");
-let bunn = iframe.contentWindow.document.getElementById("nullstill-knapp").getBoundingClientRect().top;
-iframe.style.height = bunn + 70 + "px";
-
 document.addEventListener("DOMContentLoaded", skalerIframe);
 window.addEventListener("resize", skalerIframe);
