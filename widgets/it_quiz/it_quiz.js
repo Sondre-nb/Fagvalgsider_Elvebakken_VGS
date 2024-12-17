cb1861ae924cb187d6cce96124b2d2f9510ba43d
uwohlen <112326477+uwohlen@users.noreply.github.com>
2024-12-17 01:58:31 +0100

    Update it_quiz.js

--- widgets/it_quiz/it_quiz.js
+++ widgets/it_quiz/it_quiz.js
@@ -1,6 +1,6 @@
 let spørsmålListe = [
     {"spørsmål":"Hva er en etisk utfordring ved informasjonsteknologi?", "alternativer":["Datasikkerhet", "Klassehierarki", "Feilsøking", "Programtesting"], "svar":"Datasikkerhet"},
     {"spørsmål":"Hva beskriver objektorientert modellering?", "alternativer":["Programdesign", "Programtesting", "Feilsøking", "Lagringsstandarder"], "svar":"Programdesign"},
-    {"spørsmål":"Hva er arv i programmering?", "alternativer":["Datautveksling", "Bruk av klasser", "Gjenbruk av kode", "Testing av programkode"], "svar":"Gjenbruk av kode"},
-    {"spørsmål":"Hva er en fordel med gjenbrukbar programkode?", "alternativer":["Redusert testing", "Effektiv utvikling", "Enklere feilretting", "Standardisering"], "svar":"Effektiv utvikling"},
+    {"spørsmål":"Hva er det bruk av arv i objektorientert programmering gjør mulig videre?", "alternativer":["Datautveksling", "Bruk av klasser", "Gjenbruk av kode", "Testing av programkode"], "svar":"Gjenbruk av kode"},
+    {"spørsmål":"Hva er en fordel med gjenbrukbar programkode?", "alternativer":["Redusert testing", "Effektiv utvikling", "Enklere feilretting", "Alle alternativene"], "svar":"Alle alternativene"},
 ];
